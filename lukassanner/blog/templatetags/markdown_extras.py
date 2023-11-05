14353b1dbbc90f7a7ffc80b1e5c0fea29f3b9443
Lukas Sanner <l.sanner@gmx.de>
2023-11-05 18:50:34 +0100

    update extensions

--- lukassanner/blog/templatetags/markdown_extras.py
+++ lukassanner/blog/templatetags/markdown_extras.py
@@ -5,15 +5,10 @@ import markdown as md
 
 register = template.Library()
 
+extensions = ["markdown.extensions.fenced_code", "md_mermaid", "mdx_math"]
+
 
 @register.filter
 @stringfilter
 def markdown(value):
-    return md.markdown(
-        value,
-        extensions=[
-            "markdown.extensions.fenced_code",
-            "md_mermaid",
-            "pymdownx.arithmatex",
-        ],
-    )
+    return md.markdown(value, extensions=extensions)
